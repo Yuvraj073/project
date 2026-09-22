c9245de13c4a87e83d84774816081b70218768e4
Yuvraj <yuvrajnaruka40@gmail.com>
2026-09-22 21:47:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import numpy as np 
+import pandas as pd 
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+import matplotlib.pyplot as plt
+import seaborn as sns
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout , BatchNormalization
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report,confusion_matrix
+from keras.callbacks import ReduceLROnPlateau
+import cv2
 
